bb0a7460aa3d5f1fc61223b448ca77df38ce821b
raul <raulhern927@gmail.com>
2015-07-19 23:41:13 -0500

    ok

--- NodeSlowDeep/foo.js
+++ NodeSlowDeep/foo.js
@@ -2,6 +2,6 @@
  * New node file
  */
 var _ = require('underscore');
-_.each([1, 2, 3], function(num){
+_.each([1, 2, 3, 4, 5, 6, 67, 876, 89], function(num){
 	console.log("underscore.js says " +num)
 });
